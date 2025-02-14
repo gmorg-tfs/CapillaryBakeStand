7cb11d275f2321066ccc06c3ec02dd0315f3e176
robertjaspe.spafford <robertjasper.spafford@thermofisher.com>
2025-02-14 11:25:28 -0800

    initial parsing of data file

--- rga_grapher.py
+++ rga_grapher.py
@@ -2,17 +2,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-file_path = "C:\\data\\toaster\\toaster_rga_data_77.csv"
 
-data = np.genfromtxt(file_path, delimiter=',', skip_header=1, usecols=(0, 1, 2))
-time, temp, pressure = data.T
+
+file_path = "C:\\data\\toaster\\toaster_data_10.csv"
+
+#data = np.genfromtxt(file_path, delimiter=',', skip_header=1, usecols=(0, 1, 2))
+data = np.genfromtxt(file_path, delimiter=',', skip_header=1)
+time = data[:, 0]
+pressure = data[:, 1]
+temperature = data[:, 2]
+masses = np.arange(1, 76)
+rga = data[:, 3:79]
+
+#rga mass 1 is column 0
+#rga values for mas n is column n-1
+water_17 = rga[:, 16]
+water_18 = rga[:, 17]
+water_19 = rga[:, 18]
+
+water = water_17 + water_18 + water_19
+
 
 
 fig, ax1 = plt.subplots()
 ax2 = ax1.twinx()
 import time as t
 start = t.time()
-ax1.plot(time, temp, c='r')
+ax1.plot(time, temperature, c='r')
 ax2.semilogy(time, pressure)
 plt.xticks([time[0], time[-1]])
 end = t.time()
@@ -23,4 +39,7 @@ for i in range(1, len(time)):
     dt = time[i] - time[i-1]
     a = np.append(a, [dt])
 plt.plot(a)
+plt.figure()
+plt.plot(time, water)
+
 plt.show()
